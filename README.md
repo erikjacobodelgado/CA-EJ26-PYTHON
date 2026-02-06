5ab9e60bdf97303889cb07fe7987cac2718f7245
erikjacobodelgado <erikjacobodelgado@gmail.com>
2026-02-05 23:45:32 -0600

    va

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ erikjacobodelgado@gmail.com
 ## Actividad 1
 - p001-hola-mundo.py
 - p002-area-circulo.py
-
-
+- p003-area.triangulo.py
+- p004-paga-trabajador.py
---
+++ p003-area.triangulo.py
@@ -0,0 +1,10 @@
+# p003-area.triangulo.py
+# Calcular el area de un triangulo 
+
+print('Calculando el area de un triangulo:\n')
+print('Dame la base y la altura separados por un Enter')
+
+base, altura = int(input()), int(input())
+area = base * altura / 2
+
+print(f'El triangulo de base {base} y la altura {altura} tiene un area de {area}')
