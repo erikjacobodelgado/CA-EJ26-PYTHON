f651ee2801e2ee92f74046b4e5a4f550621defab
erikjacobodelgado <erikjacobodelgado@gmail.com>
2026-02-05 23:31:51 -0600

    va

--- p001-hola-mundo.py
+++ p001-hola-mundo.py
@@ -1,4 +1,13 @@
 # p001-hola-mundo.py
 # Lee datos y envia un saludo
 
-print("Leyendo datos y enviando un saludo: ")
+print("Leyendo datos y enviando un saludo: \n")
+
+nombre = input('Dame tu nombre ')
+edad = int(input('Dame tu edad '))
+peso = float(input('Dame tu peso '))
+
+print(f'{nombre}bienvenido a python, tu edad es {edad}, tu peso es {peso}')
+print(type(nombre))
+print(type(edad))
+print(type(peso))
